520ec25363e75e4a01394809ba6f1e0695e1087e
rt01-boop <mailbox.rt01@gmail.com>
2026-03-11 16:29:40 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -47,5 +47,8 @@ def add():
     save_articles(articles)
     return {"status":"added"}
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
